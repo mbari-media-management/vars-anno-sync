ae58160acc768032d986cb9f512ca61b97066c08
Brian Schlining <bschlining@gmail.com>
2018-01-22 17:07:14 -0800

    Reconfiguring to use gradle/java 9 modules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author Brian Schlining
+ * @since 2018-01-22T16:21:00
+ */
+module org.mbari.m3.annosync {
+
+
+}
---
+++ src/main/java/org/mbari/m3/annosync/Main.java
@@ -0,0 +1,11 @@
+package org.mbari.m3.annosync;
+
+/**
+ * @author Brian Schlining
+ * @since 2018-01-22T16:10:00
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
