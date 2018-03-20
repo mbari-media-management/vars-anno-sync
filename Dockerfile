7e86fd25f7595e206d99d935d2b265400be24774
Brian Schlining <bschlining@gmail.com>
2018-03-20 13:28:10 -0700

    Setting up for deployment

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,10 @@ RUN mkdir -p ${APP_HOME}
 
 COPY build/distributions/vars-anno-sync-*.zip ${APP_HOME}
 
-RUN unzip ${APP_HOME}/*.zip && rm ${APP_HOME}/*.zip
+RUN unzip ${APP_HOME}/*.zip && \
+  rm ${APP_HOME}/*.zip && \
+  cp -R vars-anno-sync-*/ ${APP_HOME} && \
+  rm -rf vars-anno-sync-*
 
 ENTRYPOINT ${APP_HOME}/bin/vars-anno-sync
 
--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
-BUILD_HOME=`dirname "$0"`
-APP_CONF=$BUILD_HOME/src/main/resources/application.conf
-
-#scp brian@m3.shore.mbari.org:/u/brian/deployspace/m3.shore.mbari.org/vars-annotation/conf/application.conf \
-cp $HOME/workspace/m3-deployspace/m3.shore.mbari.org/vars-annotation/conf/application.conf \
-    $APP_CONF &&
-    mvnw clean package -Dmaven.test.skip=true -X
-
-if [ -f $APP_CONF ]; then
-  rm $APP_CONF
-fi
+echo "--- Building vars-anno-sync (reminder: run docker login first!!)"
+gradle assembleDist && \
+  docker build -t mbari/vars-anno-sync . && \
+  docker push mbari/vars-anno-sync
